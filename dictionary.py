e19677bfbcf0546cde0d01e1bead327b4bc55a4d
itsparsh10 <142775479+itsparsh10@users.noreply.github.com>
2023-12-18 17:46:21 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,11 @@
 import json
 from difflib import get_close_matches
-dictionary_data = json.load(open("dictionary_data.json"))
+from googletrans import Translator
 
-def translate(word):
+dictionary_data = json.load(open("dictionary_data.json"))
+translator = Translator()
+
+def translate(word, target_language='en'):
     word = word.lower()
     if word in dictionary_data:
         return dictionary_data[word]
@@ -11,23 +14,27 @@ def translate(word):
     elif word.upper() in dictionary_data:
         return dictionary_data[word.upper()]
     elif len(get_close_matches(word, dictionary_data.keys())) > 0:
-        print("did you mean %s instead" %get_close_matches(word, dictionary_data.keys())[0])
-        decide = input("press y for yes or n for no: ")
+        print("Did you mean %s instead?" % get_close_matches(word, dictionary_data.keys())[0])
+        decide = input("Press y for yes or n for no: ")
         if decide == "y":
             return dictionary_data[get_close_matches(word, dictionary_data.keys())[0]]
         elif decide == "n":
-            return("pugger your paw steps on working keys ")
+            return "Pugger, your paw steps on working keys."
         else:
-            return("You have entered wrong input please enter just y or n")    
+            return "You have entered the wrong input. Please enter just y or n."    
     else:
-        print("You have entered wrong keys. Try again again ")
+        print("You have entered wrong keys. Try again.")
         
 
-
 word = input("Enter the word you want to search: ")
+target_language = input("Enter the target language (e.g., 'fr' for French, 'es' for Spanish): ")
+
 output = translate(word)
+
 if type(output) == list:
     for item in output:
-        print(item)
+        translated_item = translator.translate(item, dest=target_language).text
+        print(translated_item)
 else:
-    print(output)
+    translated_output = translator.translate(output, dest=target_language).text
+    print(translated_output)
